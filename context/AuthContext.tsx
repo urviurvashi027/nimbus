80829b1cc8bf456505948ff57c65ec3e17d3916f
Urvashi Urvi <urvi@Urvashis-MacBook-Pro.local>
2025-08-23 19:07:43 +0530

    added +91 in register flow

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -110,7 +110,7 @@ export default function AuthProvider({ children }: { children: ReactNode }) {
       const request = {
         username: userName,
         email,
-        mobile: `+91${mobile}`,
+        phone_number: `+91${mobile}`,
         full_name: fullName,
         password,
       };
